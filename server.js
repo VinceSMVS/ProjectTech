f2236cb36b09bcd950ce66cd498a33daa89bcd7e
VinceSMVS <vincevanschagen@gmail.com>
2025-02-13 12:49:38 +0100

    add ejs

--- server.js
+++ server.js
@@ -4,25 +4,29 @@ const path = require('path');
 
 
 app.set("view engine", "ejs")
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'home.html'));
-  });
+  res.render("pages/index")
+})
 
 app.get('/about', (req, res) => {
-    res.render("about")
+    res.render("pages/about")
 })
 app.get('/login', (req, res) => {
-    res.render("login")
+    res.render("pages/login")
+})
+app.get('/form', (req, res) => {
+  res.render("pages/form")
 })
 
-app.use(express.static('public'))
 
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
   })
 
-app.listen(3000)
+app.listen(3000, () => {
+  console.log('server is actief')
+})
 
 
-// Serve static files: Zoals CSS en afbeeldingen.
