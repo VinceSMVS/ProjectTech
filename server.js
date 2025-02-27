cfa76c8d074f4880c6e59a9aa2b82d8088bbc863
VinceSMVS <vincevanschagen@gmail.com>
2025-02-27 14:53:56 +0100

    init mongoDB

--- server.js
+++ server.js
@@ -5,56 +5,56 @@ const port = 3000;
 require('dotenv').config();
 
 const { MongoClient } = require("mongodb");
-// Replace the uri string with your MongoDB deployment's connection string.
 const uri = process.env.URI;
 const client = new MongoClient(uri);
-async function run() {
-  try {
-    await client.connect();
-    // database and collection code goes here
-    // find code goes here
-    // iterate code goes here
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
-
 
 app.set("view engine", "ejs")
 app.use(express.static('public'))
-app.use(express.urlencoded({ extendend: true })) //dit is middelware om te zorgen dat ik informatie van forms kan ophalen.
+app.use(express.urlencoded({ extendend: true })) 
 
+//mongoDB setup
+async function run() {
+  try {
+    await client.connect();
+    console.log("MongoDB connected")
+    const db = client.db("sample_mflix");
+    return db; 
+    const coll = db.collection("movies");
+    const cursor = coll.find();
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+    throw error;
+  }
+}
+run().catch(console.dir);
 
 //Get routes
 app.get('/', (req, res) => {
   res.render("pages/index")
 })
-
 app.get('/about', (req, res) => {
     res.render("pages/about")
 })
-
 app.get('/login', (req, res) => {
     res.render("pages/login", { username: "test" })
 })
-
 app.get('/register', (req, res) => {
   res.render("pages/register")
 })
-
 app.get('/welcome', (req, res) => {
   const username = req.query.username;
   res.render("pages/welcome", { username: username })
-
 })
 
 //Post routes
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
   const username = req.body.username;
   res.redirect(`/welcome?username=${username}`);
+  const db = await run(); 
+  const collection = db.collection('biepdiebapo'); 
+  const dataToInsert = {username: username};
+  await collection.insertOne(dataToInsert); 
+  console.log('Form data inserted:', dataToInsert);
 });
 
 //Error handling
